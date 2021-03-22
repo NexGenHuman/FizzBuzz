c4c122ccc56e20257847fd379cec77823c09033f
Konrad Wasilewski <konradwasilewski@vp.pl>
2021-03-22 22:31:01 +0100

    Created empty "Home" and "Recently_Searched" pages

---
+++ FizzBuzz/FizzBuzz/Pages/Recently_Searched.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FizzBuzz.Pages
+{
+    public class Recently_SearchedModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
