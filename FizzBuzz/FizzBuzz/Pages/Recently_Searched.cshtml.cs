353545c4242f44de1da9fc52bc70502a8f9e6e0d
Konrad Wasilewski <konradwasilewski@vp.pl>
2021-04-13 02:04:16 +0200

    Prototype reading from DB

--- FizzBuzz/FizzBuzz/Pages/Recently_Searched.cshtml.cs
+++ FizzBuzz/FizzBuzz/Pages/Recently_Searched.cshtml.cs
@@ -7,15 +7,63 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using FizzBuzz.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Data.SqlClient;
 
 namespace FizzBuzz.Pages
 {
     public class Recently_SearchedModel : PageModel
     {
-        public List<FizzBuzz_Data> FBlist = new List<FizzBuzz_Data>();
+        public string lblInfoText;
+
+        private readonly ILogger<Recently_SearchedModel> _logger;
+        public IConfiguration _configuration { get; }
+        public Recently_SearchedModel(IConfiguration configuration, ILogger<Recently_SearchedModel> logger)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        //public List<FizzBuzz_Data> FBlist = new List<FizzBuzz_Data>();
 
         public void OnGet()
         {
+            string FizzBuzzDBcs = ConfigurationExtensions.GetConnectionString(this._configuration, "FizzBuzzDB");
+
+            SqlConnection con = new SqlConnection(FizzBuzzDBcs);
+            string sql = "SELECT * FROM FizzBuzzRecent";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            StringBuilder htmlStr = new StringBuilder("");
+            while (reader.Read())
+            {
+                htmlStr.Append("<div class=\"text-center row\">");
+                htmlStr.Append("<div class=\"col-md-4\">");
+                htmlStr.Append("<p>");
+                htmlStr.Append(reader["output"].ToString() + " ");
+                htmlStr.Append("</p>");
+                htmlStr.Append("</div>");
+                htmlStr.Append("<div class=\"col-md-4\">");
+                htmlStr.Append("<p>");
+                htmlStr.Append(reader["input"].ToString() + " ");
+                htmlStr.Append("</p>");
+                htmlStr.Append("</div>");
+                htmlStr.Append("<div class=\"col-md-4\">");
+                htmlStr.Append("<p>");
+                htmlStr.Append(reader["date"].ToString() + " ");
+                htmlStr.Append("</p>");
+                htmlStr.Append("</div>");
+                htmlStr.Append("</div>");
+            }
+
+            reader.Close();
+            con.Close();
+            lblInfoText = htmlStr.ToString();
+
+            /*
             var SessionList = HttpContext.Session.GetString("SessionList");
             if (SessionList != null)
                 FBlist = JsonConvert.DeserializeObject<List<FizzBuzz_Data>>(SessionList);
@@ -28,6 +76,7 @@ namespace FizzBuzz.Pages
             }
 
             HttpContext.Session.SetString("SessionList", JsonConvert.SerializeObject(FBlist));
+            */
         }
     }
 }
---
+++ FizzBuzz/SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿DROP PROCEDURE sp_FizzBuzzAdd
+GO
+
+CREATE PROCEDURE[dbo].[sp_FizzBuzzAdd]
+	@input INT,
+	@output NCHAR(10),
+	@date DATETIME,
+	@Id INT OUTPUT
+AS
+	INSERT INTO FizzBuzzRecent(input, output, date) VALUES (@input, @output, @date)
+	SET @Id = @@IDENTITY
